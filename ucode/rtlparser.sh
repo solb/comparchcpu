1a805a1bce472f408cf0ff2374967133acc532bb
Sol Boucher <slb1566@rit.edu>
2014-04-26 17:35:05 -0400

    Extend jump table generator to also assign some control word opcodes

--- ucode/rtlparser.sh
+++ ucode/rtlparser.sh
@@ -2,15 +2,17 @@
 
 # Sol Boucher and Evan Klei
 # CSCI-453-01 Computer Architecture
-# FabComp: intermediate micro--jump table generator
+# FabComp: intermediate micro--jump table and control word micro-opcode generator
 
 jumptable="jmptab.tl" # output file
+ctrlwords="ctrlwords.cwt" # output file
 
 if [ $# -ne 1 ] ; then
 	cat <<-EOM
 		USAGE: $0 <hardware reference document>
 		The hardware reference document should end in the extension .tex
 		The generated intermediate table will be named $jumptable
+		The generated control words table will be named $ctrlwords
 	EOM
 	exit 1
 fi
@@ -18,6 +20,8 @@ fi
 listing=`sed -n '/BEGIN RTL/,/END RTL/p' "$1" | sed -re '/^$/d' -e 's/^[ \t]+//' -e '/\{.*\}/d' -e '/^%/d' -e 's/([^[:space:]]) #.*$/\1/' -e '/^# [A-Z].*$/d'`
 labels=`echo "$listing" | sed -ne '/:/p' -e '/goes here/p'`
 echo "$labels" >"$jumptable"
+words=`echo "$listing" | sed -e '/:/d' -e '/goes here/d' -e '/^#/d' -e '/if /d' -e '/else/d' | sort | uniq`
+truncate -s 0 "$ctrlwords"
 
 address=0
 echo "$listing" | while read line ; do
@@ -26,6 +30,12 @@ echo "$listing" | while read line ; do
 		echo "processing: $name"
 		sed -i "s/.*\(\<$name\>\).*/`printf %x $address`\t\1/" "$jumptable"
 	elif ! ( echo "$line" | grep "goes here" >/dev/null 2>&1 ) ; then
-		address=$(($address+1))
+		address=$(($address + 1))
 	fi
+done
+
+uopcode=0
+echo "$words" | while read line ; do
+	printf '%02x\t%s\n' "$uopcode" "$line" >>"$ctrlwords"
+	uopcode=$(($uopcode + 1))
 done
