cc9ecb97af6501482aa2049422b41e32278cc2ff
Sol Boucher <slb1566@rit.edu>
2014-04-26 22:52:49 -0400

    Add labels for whiles, elses, and fis when generating intermediate symbol table

--- ucode/rtlparser.sh
+++ ucode/rtlparser.sh
@@ -7,6 +7,12 @@
 jumptable="jmptab.tl" # output file
 ctrlwords="ctrlwords.cwt" # output file
 
+# Handles special labels provided by RTL itself (namely whiles, elses, and fis)
+processlangbranch() {
+	echo "processing: $name"
+	printf '%x\t%s\n' $address "$name" >>"$jumptable"
+}
+
 if [ $# -ne 1 ] ; then
 	cat <<-EOM
 		USAGE: $0 <hardware reference document>
@@ -17,18 +23,43 @@ if [ $# -ne 1 ] ; then
 	exit 1
 fi
 
-listing=`sed -n '/BEGIN RTL/,/END RTL/p' "$1" | sed -re '/^$/d' -e 's/^[ \t]+//' -e '/\{.*\}/d' -e '/^%/d' -e 's/([^[:space:]]) #.*$/\1/' -e '/^# [A-Z].*$/d'`
+# labels and "belongs here"s belong in the jump table
+# elifs, elses, fis, and whiles belong in the jump table
+# ifs, elses, and dones count as an instruction
+# elifs count as two instructions (since they include a jump and a branch)
+
+listing=`sed -n '/BEGIN RTL/,/END RTL/p' "$1" | sed -re '/^$/d' -e 's/^[ \t]+//' -e '/\{.*\}/d' -e '/^%/d' -e 's/([^[:space:]]) #.*$/\1/' -e '/^# [A-Z].*$/d' -e '/elif/ielse'`
 labels=`echo "$listing" | sed -ne '/:/p' -e '/goes here/p'`
 echo "$labels" >"$jumptable"
-words=`echo "$listing" | sed -e '/:/d' -e '/goes here/d' -e '/^#/d' -e '/if /d' -e '/else/d' | sort | uniq`
+words=`echo "$listing" | sed -e '/:/d' -e '/goes here/d' -e '/^#/d' -e '/if/d' -e '/else/d' -e '/fi/d' -e '/while/d' -e '/done/d' | sort | uniq`
 truncate -s 0 "$ctrlwords"
+#listing=`echo "$listing" | sed '/elif/ielse'` TODO remove
+
+#echo "$listing"
+#exit 2
 
 address=0
+whiles=0
+elses=0
+fis=0
 echo "$listing" | while read line ; do
 	if ( echo "$line" | grep ":" >/dev/null 2>&1 ) ; then
 		name=`echo "$line" | sed 's/:*$//'`
 		echo "processing: $name"
 		sed -i "s/.*\(\<$name\>\).*/`printf %x $address`\t\1/" "$jumptable"
+	elif ( echo "$line" | grep "while" >/dev/null 2>&1 ) ; then
+		name="while$whiles"
+		processlangbranch
+		whiles=$(($whiles + 1))
+	elif ( echo "$line" | grep "else" >/dev/null 2>&1 ) ; then
+		name="else$elses"
+		address=$(($address + 1))
+		processlangbranch
+		elses=$(($elses + 1))
+	elif ( echo "$line" | grep "fi" >/dev/null 2>&1 ) ; then
+		name="fi$fis"
+		processlangbranch
+		fis=$(($fis + 1))
 	elif ! ( echo "$line" | grep "goes here" >/dev/null 2>&1 ) ; then
 		address=$(($address + 1))
 	fi
