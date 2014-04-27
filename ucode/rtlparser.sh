73237cc0df4b14a89269915e4f6f1ee11956059f
Sol Boucher <slb1566@rit.edu>
2014-04-26 23:04:01 -0400

    Make RTL parser output microcode precurser in addition to intermediate jump table

--- ucode/rtlparser.sh
+++ ucode/rtlparser.sh
@@ -4,13 +4,19 @@
 # CSCI-453-01 Computer Architecture
 # FabComp: intermediate micro--jump table and control word micro-opcode generator
 
-jumptable="jmptab.tl" # output file
-ctrlwords="ctrlwords.cwt" # output file
+# output filenames
+microcode="ucode.tu"
+jumptable="jmptab.tl"
+ctrlwords="ctrlwords.cwt"
 
 # Handles special labels provided by RTL itself (namely whiles, elses, and fis)
 processlangbranch() {
-	echo "processing: $name"
-	printf '%x\t%s\n' $address "$name" >>"$jumptable"
+	echo "processing: $1$2"
+	if "$3" ; then
+		address=$(($address + 1))
+		echo "$line" | sed -e "s/$1/&$2/" >>"$microcode"
+	fi
+	printf '%x\t%s\n' $address "$1$2" >>"$jumptable"
 }
 
 if [ $# -ne 1 ] ; then
@@ -29,41 +35,36 @@ fi
 # elifs count as two instructions (since they include a jump and a branch)
 
 listing=`sed -n '/BEGIN RTL/,/END RTL/p' "$1" | sed -re '/^$/d' -e 's/^[ \t]+//' -e '/\{.*\}/d' -e '/^%/d' -e 's/([^[:space:]]) #.*$/\1/' -e '/^# [A-Z].*$/d' -e '/elif/ielse'`
+truncate -s 0 "$microcode"
 labels=`echo "$listing" | sed -ne '/:/p' -e '/goes here/p'`
 echo "$labels" >"$jumptable"
-words=`echo "$listing" | sed -e '/:/d' -e '/goes here/d' -e '/^#/d' -e '/if/d' -e '/else/d' -e '/fi/d' -e '/while/d' -e '/done/d' | sort | uniq`
-truncate -s 0 "$ctrlwords"
-#listing=`echo "$listing" | sed '/elif/ielse'` TODO remove
-
-#echo "$listing"
-#exit 2
 
 address=0
 whiles=0
 elses=0
 fis=0
 echo "$listing" | while read line ; do
-	if ( echo "$line" | grep ":" >/dev/null 2>&1 ) ; then
+	if ( echo "$line" | grep "while" >/dev/null 2>&1 ) ; then
+		processlangbranch while $whiles false
+		whiles=$(($whiles + 1))
+	elif ( echo "$line" | grep "else" >/dev/null 2>&1 ) ; then
+		processlangbranch else $elses true
+		elses=$(($elses + 1))
+	elif ( echo "$line" | grep "\<fi\>" >/dev/null 2>&1 ) ; then
+		processlangbranch fi $fis false
+		fis=$(($fis + 1))
+	elif ( echo "$line" | grep ":" >/dev/null 2>&1 ) ; then
 		name=`echo "$line" | sed 's/:*$//'`
 		echo "processing: $name"
 		sed -i "s/.*\(\<$name\>\).*/`printf %x $address`\t\1/" "$jumptable"
-	elif ( echo "$line" | grep "while" >/dev/null 2>&1 ) ; then
-		name="while$whiles"
-		processlangbranch
-		whiles=$(($whiles + 1))
-	elif ( echo "$line" | grep "else" >/dev/null 2>&1 ) ; then
-		name="else$elses"
-		address=$(($address + 1))
-		processlangbranch
-		elses=$(($elses + 1))
-	elif ( echo "$line" | grep "fi" >/dev/null 2>&1 ) ; then
-		name="fi$fis"
-		processlangbranch
-		fis=$(($fis + 1))
 	elif ! ( echo "$line" | grep "goes here" >/dev/null 2>&1 ) ; then
+		echo "$line" >>"$microcode"
 		address=$(($address + 1))
 	fi
 done
+
+words=`echo "$listing" | sed -e '/:/d' -e '/goes here/d' -e '/^#/d' -e '/if/d' -e '/else/d' -e '/fi/d' -e '/while/d' -e '/done/d' | sort | uniq`
+truncate -s 0 "$ctrlwords"
 
 uopcode=0
 echo "$words" | while read line ; do
