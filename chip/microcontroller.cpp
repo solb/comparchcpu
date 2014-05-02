f45747407edeb252c3abbc7574832bc3506086df
Sol Boucher <slb1566@rit.edu>
2014-05-01 22:57:27 -0400

    Add space in C++ code for implementing hardware control points
    The individual cases still need to be filled in

--- chip/microcontroller.cpp
+++ chip/microcontroller.cpp
@@ -21,6 +21,9 @@
 using std::cerr;
 using std::endl;
 
+// Set the group of control points necessary to effect a line of RTL
+static void execute_rtl(unsigned control_points);
+
 // Is the conditional described by the current microinstruction satisfied?
 // Precondition: We must currently be looking at a J-type microinstruction!
 static bool decide_conditional();
@@ -44,7 +47,8 @@ void interpret_microprogram() {
 		// See hardware specification, section 3.2, first table
 		switch(uinst_type()) {
 			case 0x0: // C-type
-				// TODO Implement control point setting
+				// Set the control points to make that happen in the datapath
+				execute_rtl(uinst_c_ctrl());
 				break;
 
 			case 0xa: // Branch -> uJumpTab
@@ -120,6 +124,223 @@ void interpret_microprogram() {
 						uinst_type() << endl;
 				proceed = false;
 		}
+	}
+}
+
+static void execute_rtl(unsigned control_points) {
+	// See hardware specification, section 3.1 table
+	// TODO Populate these cases
+	switch(control_points) {
+		case 0x00:
+			break;
+
+		case 0x01:
+			break;
+
+		case 0x02:
+			break;
+
+		case 0x03:
+			break;
+
+		case 0x04:
+			break;
+
+		case 0x05:
+			break;
+
+		case 0x06:
+			break;
+
+		case 0x07:
+			break;
+
+		case 0x08:
+			break;
+
+		case 0x09:
+			break;
+
+		case 0x0a:
+			break;
+
+		case 0x0b:
+			break;
+
+		case 0x0c:
+			break;
+
+		case 0x0d:
+			break;
+
+		case 0x0e:
+			break;
+
+		case 0x0f:
+			break;
+
+		case 0x10:
+			break;
+
+		case 0x11:
+			break;
+
+		case 0x12:
+			break;
+
+		case 0x13:
+			break;
+
+		case 0x14:
+			break;
+
+		case 0x15:
+			break;
+
+		case 0x16:
+			break;
+
+		case 0x17:
+			break;
+
+		case 0x18:
+			break;
+
+		case 0x19:
+			break;
+
+		case 0x1a:
+			break;
+
+		case 0x1b:
+			break;
+
+		case 0x1c:
+			break;
+
+		case 0x1d:
+			break;
+
+		case 0x1e:
+			break;
+
+		case 0x1f:
+			break;
+
+		case 0x20:
+			break;
+
+		case 0x21:
+			break;
+
+		case 0x22:
+			break;
+
+		case 0x23:
+			break;
+
+		case 0x24:
+			break;
+
+		case 0x25:
+			break;
+
+		case 0x26:
+			break;
+
+		case 0x27:
+			break;
+
+		case 0x28:
+			break;
+
+		case 0x29:
+			break;
+
+		case 0x2a:
+			break;
+
+		case 0x2b:
+			break;
+
+		case 0x2c:
+			break;
+
+		case 0x2d:
+			break;
+
+		case 0x2e:
+			break;
+
+		case 0x2f:
+			break;
+
+		case 0x30:
+			break;
+
+		case 0x31:
+			break;
+
+		case 0x32:
+			break;
+
+		case 0x33:
+			break;
+
+		case 0x34:
+			break;
+
+		case 0x35:
+			break;
+
+		case 0x36:
+			break;
+
+		case 0x37:
+			break;
+
+		case 0x38:
+			break;
+
+		case 0x39:
+			break;
+
+		case 0x3a:
+			break;
+
+		case 0x3b:
+			break;
+
+		case 0x3c:
+			break;
+
+		case 0x3d:
+			break;
+
+		case 0x3e:
+			break;
+
+		case 0x3f:
+			break;
+
+		case 0x40:
+			break;
+
+		case 0x41:
+			break;
+
+		case 0x42:
+			break;
+
+		case 0x43:
+			break;
+
+		case 0x44:
+			break;
+
+		default:
+			emergency_halt("execute_rtl()",
+					"Invalid microinstruction control point identifier");
 	}
 }
 
